0cb876d11d6b7c5c43e45470d7d2d430ff12c49a
tommyh2o <tommyhuynh02n@gmail.com>
2024-11-07 16:37:52 -0600

    experience

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <nav>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/portfolio">Experience</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
