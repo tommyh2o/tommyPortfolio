e8af5de076329c24efaa7831bf87579c9d96e574
tommyh2o <tommyhuynh02n@gmail.com>
2024-11-07 13:48:36 -0600

    Home page update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/header';
-import Portfolio from './components/Portfolio';
-import About from './components/About';
-import Contact from './components/Contact';
-import './styles.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Parallax } from "react-parallax";
+import Header from "./components/header";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Contact from "./components/contact";
+import SeattleSkyline from "./img/SeattleSkyline.jpg";
+import profilePic from "./img/homePhoto.jpg";
+import "./styles.css";
 
 const App = () => {
   return (
@@ -24,9 +27,36 @@ const App = () => {
 
 const Home = () => (
   <div className="container">
-    <section id="home">
-      <h1>Welcome to My Portfolio</h1>
-      <p>Explore my projects and learn more about my work.</p>
+    <Parallax bgImage={SeattleSkyline} strength={300}>
+      <div style={{ height: "500px" }}>
+        <section id="home" className="parallax-content">
+          <div className="hero">
+            <h1>Hello, welcome to my personal portfolio!</h1>
+          </div>
+        </section>
+      </div>
+    </Parallax>
+
+    <section className="about-section">
+      <img src={profilePic} alt="Tommy Q Huynh" className="about-photo" />
+      <div className="about-content">
+        <h2>Who am I?</h2>
+        <p>
+          Hello! I'm Tommy Huynh, a recent graduate from the University of Texas
+          at Austin (May 2024) with a strong passion for coding and software
+          development. I’m excited to break into the tech industry, especially
+          in Software Development and Engineering roles, though I'm open to
+          opportunities that fit my skills. My commitment to customer
+          satisfaction, meeting deadlines, and continuously improving my skills
+          drives me to excel in every project. I believe my eagerness to learn
+          and the experiences I've gathered so far make me a strong candidate
+          for roles in fast-paced, innovative environments. Feel free to click
+          the link below to learn more about my journey and skills!
+        </p>
+        <Link to="/about" className="about-link">
+          My about page.
+        </Link>
+      </div>
     </section>
   </div>
 );
